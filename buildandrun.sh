84d70b692a36c65d8c44d7290b4e96ef6b6f2ab8
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-02-23 02:14:59 -0500

    updated ssh file

--- buildandrun.sh
+++ buildandrun.sh
@@ -40,7 +40,7 @@ sudo chmod -R 775 /home/ec2-user/webapp
 sudo chown -R ec2-user:ec2-user /home/ec2-user/webapp
 
 mvn clean install -f /home/ec2-user/webapp/pom.xml
-nohup & java -jar /home/ec2-user/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+nohup  java -jar /home/ec2-user/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar &
 
 sudo useradd mahesh
 sudo passwd mahesh
