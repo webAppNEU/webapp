3fbaa43f2ebd76aae12415c856ab18d8ac38806a
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-02-23 01:29:05 -0500

    updated packer file

--- buildandrun.sh
+++ buildandrun.sh
@@ -36,8 +36,11 @@ export PATH
 #sudo yum install git -y
 #git clone https://maheshpoojaryneu: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@github.com/webAppNEU/webapp.git ~/webapp
 
-#mvn clean install -f /home/ec2-user/webapp/pom.xml
-#nohup & java -jar /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+sudo chmod -R 775 /home/ec2-user/webapp
+sudo chown -R ec2-user:ec2-user /home/ec2-user/webapp
+
+mvn clean install -f /home/ec2-user/webapp/webapp/pom.xml
+nohup & java -jar /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
 
 sudo useradd mahesh
 sudo passwd mahesh
@@ -45,13 +48,11 @@ sudo passwd mahesh
 #sudo chmod 500 /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
 
 #sudo chown mahesh:mahesh ~/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
-sudo chmod -R 775 /home/ec2-user/webapp
-sudo chown -R ec2-user:ec2-user /home/ec2-user/webapp
 #mvn clean install -f /home/ec2-user/webapp/pom.xml
 #sudo chown mahesh:mahesh ~/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
 #sudo chmod -R 775 /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
 #sudo chmod 777 /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
 #sudo chmod 775 /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
-cat /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
-sudo nohup& java -jar /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
+#cat /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
+#sudo nohup& java -jar /home/ec2-user/webapp/UserWebApp-0.0.1-SNAPSHOT.jar
 
