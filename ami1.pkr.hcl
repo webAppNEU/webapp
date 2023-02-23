eda37c1525643115298dd770524c7fa3f4af407b
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-02-22 22:37:01 -0500

    packer build and env variables

---
+++ ami1.pkr.hcl
@@ -0,0 +1,80 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "source_ami" {
+  type    = string
+  default = "ami-0dfcb1ef8550277af" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+}
+variable "ssh_username" {
+  type    = string
+  default = "ec2-user"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-0978f277d99ea40ae"
+}
+// variable "profile" {
+//   type    = string
+//   default = "dev"
+// }
+
+# https://www.packer.io/plugins/builders/amazon/ebs
+source "amazon-ebs" "my-ami" {
+  region          = "us-east-1"
+  ami_name        = "csye6225_${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
+  ami_description = "AMI for CSYE 6225"
+  #  profile         = "${var.profile}"
+  profile = "ami"
+
+  ami_regions = [
+    "us-east-1",
+  ]
+
+
+
+  // aws_polling {
+  //   delay_seconds = 120
+  //   max_attempts  = 50
+  // }
+
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  source_ami                  = "${var.source_ami}"
+  subnet_id                   = "${var.subnet_id}"
+
+  // ssh_handshake_attempts = 200
+  #security_group_id      = "sg-0acb65bd8a41f9767"
+  #ssh_keypair_name       = "ec2test"
+  #ssh_private_key_file   = "~/.ssh/ec2test"
+
+  launch_block_device_mappings {
+    delete_on_termination = true
+    device_name           = "/dev/xvda"
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+
+  ssh_username = "${var.ssh_username}"
+
+}
+build {
+  sources = ["source.amazon-ebs.my-ami"]
+
+
+  provisioner "file" {
+    source      = "./"
+    destination = "~/webapp/"
+  }
+
+  provisioner "shell" {
+    environment_vars = [
+      "DEBIAN_FRONTEND=noninteractive",
+      "CHECKPOINT_DISABLE=1"
+    ]
+    script       = "./buildandrun.sh"
+    pause_before = "3s"
+  }
+
+}
---
+++ buildandrun.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+sudo yum update -y
+sudo yum install -y java-17-amazon-corretto
+sudo yum install -y httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+
+mkdir mysql
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm -P ~/mysql
+sudo yum localinstall -y ~/mysql/mysql57-community-release-el7-8.noarch.rpm 
+sudo yum install -y mysql-community-server
+
+sudo systemctl stop mysqld
+sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+sudo systemctl start mysqld
+mysql -u root -D mysql -e "UPDATE user SET authentication_string = PASSWORD('root') WHERE User = 'root';"
+mysql -u root -D mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER root@'localhost' IDENTIFIED BY 'root'" -proot --connect-expired-password;
+#mysql -u root -e "SET GLOBAL validate_password.length = 4" ;
+mysql -u root -e "CREATE DATABASE userwebapp;" -proot --connect-expired-password;
+sudo systemctl stop mysqld
+sudo systemctl unset-environment MYSQLD_OPTS
+sudo systemctl start mysqld
+
+
+mkdir ~/maven/
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.tar.gz -P ~/maven
+sudo tar xf ~/maven/apache-maven-3.9.0-bin.tar.gz -C ~/maven
+M2_HOME='/home/ec2-user/maven/apache-maven-3.9.0'
+PATH="$M2_HOME/bin:$PATH"
+export PATH
+
+mkdir ~/webapp
+#sudo yum install git -y
+#git clone https://maheshpoojaryneu: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@github.com/webAppNEU/webapp.git ~/webapp
+
+#mvn clean install -f /home/ec2-user/webapp/pom.xml
+#nohup & java -jar /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+
+sudo useradd mahesh
+sudo passwd mahesh
+#sudo chown mahesh:mahesh /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+#sudo chmod 500 /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+
+sudo chown mahesh:mahesh ~/webapp
+sudo chmod 500 ~/webapp
+mvn clean install -f /home/ec2-user/webapp/pom.xml
+sudo chown mahesh:mahesh /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+sudo chmod 500 /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+nohup & java -jar /home/ec2-user/webapp/webapp/target/UserWebApp-0.0.1-SNAPSHOT.jar
+
