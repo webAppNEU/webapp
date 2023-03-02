c81a1c9109db3c5293a4a8d95a29cb1392e43c3e
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-03-02 05:16:32 -0500

    updated application code

--- src/main/java/com/webapp/userwebapp/controller/awscredentials.java
+++ src/main/java/com/webapp/userwebapp/controller/awscredentials.java
@@ -1,20 +1,20 @@
-//package com.webapp.userwebapp.controller;
-//
-//import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-//import com.amazonaws.regions.Regions;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import com.amazonaws.auth.AWSCredentials;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//@Configuration
-//public class awscredentials {
-//
+package com.webapp.userwebapp.controller;
+
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class awscredentials {
+
 //@Bean
 //public AmazonS3 s3client(){
 //
@@ -27,27 +27,34 @@
 //            .build();
 //    return s3Client;
 //}
+
+    @Bean
+    public AmazonS3 s3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+//    @Value("${aws.access.key.id}")
+//    private String awsId;
 //
-////    @Value("${aws.access.key.id}")
-////    private String awsId;
-////
-////    @Value("${aws.secret.access.key}")
-////    private String awsKey;
-////
-////    @Value("${aws.s3.region}")
-////    private String region;
-////
-////    @Bean
-////    public AmazonS3 s3client() {
-////
-////        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
-////        AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
-////                .withRegion(Regions.fromName(region))
-////                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-////                .build();
-////
-////        return amazonS3Client;
-////
-////
-////    }
-//}
+//    @Value("${aws.secret.access.key}")
+//    private String awsKey;
+//
+//    @Value("${aws.s3.region}")
+//    private String region;
+//
+//    @Bean
+//    public AmazonS3 s3client() {
+//
+//        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
+//        AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
+//                .withRegion(Regions.fromName(region))
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                .build();
+//
+//        return amazonS3Client;
+//
+//
+//    }
+}
