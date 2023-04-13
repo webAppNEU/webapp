8e671c184df3e0da3a71e9e6eded227f9fd7d4a6
maheshpoojaryneu <poojary.m@northeastern.edu>
2023-04-12 21:46:02 -0400

    Updated packer

--- src/test/java/com/webapp/userwebapp/UnitTest.java
+++ src/test/java/com/webapp/userwebapp/UnitTest.java
@@ -1,14 +1,21 @@
 package com.webapp.userwebapp;
 
+import com.timgroup.statsd.StatsDClient;
 import com.webapp.userwebapp.controller.UserOps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+
+import static org.mockito.Mockito.*;
+
 
 public class UnitTest {
 
-    @Test
 
+
+    @Test
     void test_healthz_url()
     {
         UserOps userOps = new UserOps();
